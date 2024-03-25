3f020674460c7c499fe131e4355f4233a93137a4
Yukyu-ef <butamanjuukaihougun001@gmail.com>
2024-03-25 10:14:50 +0900

    ドクターズモバイル試験用

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import Enumerable from "linq";
 
-console.log(hello("TypeScript"));
+// console.log(Enumerable.range(0, 100).sum());
+
+const calcMethod = () => {
+  let n = 0;
+
+  for (let i = 0; i < 101; i++) {
+    n = n + i;
+  }
+  return n;
+};
+
+console.log(calcMethod());
